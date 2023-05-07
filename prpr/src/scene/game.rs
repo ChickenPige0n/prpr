b0bd5008653ee2008be82ae5c31e15d81c64ae83
ChickenPige0n <2336983354@qq.com>
2023-05-07 16:40:04 +0800

    forgot to retore the lines,,

--- prpr/src/scene/game.rs
+++ prpr/src/scene/game.rs
@@ -395,7 +395,7 @@ impl GameScene {
         });
         if self.judge.combo() >= 3 {
             let btm = self.chart.with_element(ui, res, UIElement::ComboNumber, |ui, color, scale| {
-                ui.text("3")//self.judge.combo().to_string())
+                ui.text(self.judge.combo().to_string())
                     .pos(0., top + eps * 1.346 - (1. - p) * 0.4)
                     .anchor(0.5, 0.)
                     .color(Color { a: color.a * c.a, ..color })
@@ -404,7 +404,7 @@ impl GameScene {
                     .bottom()
             });
             self.chart.with_element(ui, res, UIElement::Combo, |ui, color, scale| {
-                ui.text(if res.config.autoplay { "COMBO" } else { "COMBO" })
+                ui.text(if res.config.autoplay { "AUTOPLAY" } else { "COMBO" })
                     .pos(0., btm + 0.007777)
                     .anchor(0.5, 0.)
                     .size(0.325)
